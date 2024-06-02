03939c843ad4185107e5c2847c20c0b802012cd0
Naji Aboo <aboonaji@gmail.com>
2024-06-02 20:51:30 +0530

    added the requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Generative AI Project",
+    packages=find_packages(),
+    version="0.1.0",
+    author="Mohamed Naji Aboo"
+    
+    )
